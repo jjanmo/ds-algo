a85deb4df6fa3938c238370543b048a1ec3a74d3
jjanmo <jjanmo07@hanmail.net>
2020-09-16 17:32:26 +0900

    Solve problem3

--- basic/integer-reversal.js
+++ basic/integer-reversal.js
@@ -1,1 +1,33 @@
-function reverseNumber(number) {}
+//주어진 숫자를 거꾸로 출력하시오
+
+//1234 => 4321
+//-1200 => -21
+
+function reverseNumber(number) {
+	const numberStr = String(number);
+	let newNumberStr = '';
+	let isFirstZero = true;
+
+	for (let i = numberStr.length - 1; i >= 0; i--) {
+		if (numberStr[i] === '-') {
+			newNumberStr = '-' + newNumberStr;
+			return newNumberStr;
+		} else if (numberStr[i] !== '0') {
+			//0 아닌 경우
+			isFirstZero = false;
+			newNumberStr += numberStr[i];
+		} else {
+			//0인 경우
+			if (!isFirstZero) {
+				//첫번째 0이 아닌 경우
+				newNumberStr += numberStr[i];
+			}
+		}
+	}
+
+	return Number(newNumberStr);
+}
+
+console.log(reverseNumber(1234));
+console.log(reverseNumber(-1200));
+console.log(reverseNumber(14200300));
---
+++ basic/integer-reversal.md
@@ -0,0 +1,11 @@
+# Integer-reversal
+
+> **문제)** `숫자`를 입력받고 그것의 `거꾸로 된 숫자`를 반환하시오
+
+# WIL(What I Learned this problem)
+
+> 이 문제를 통해서 알게 된 사실들에 대해 간단하게 정리한다.
+
+## Number() vs parseInt()
+
+## Math.sign(x)
