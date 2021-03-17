b3489f43b362a24c6dd4bfd550a0c4ff3413e994
jjanmo <jjanmo07@hanmail.net>
2021-03-18 01:03:47 +0900

    Solve section01-problem08

--- algorithm/section01/s01_08.js
+++ algorithm/section01/s01_08.js
@@ -1,3 +1,19 @@
-function solution(arr) {}
+function solution(array) {
+  const sum = array.reduce((acc, cur) => acc + cur);
+  const diff = sum - 100;
+  let result;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (diff === array[i] + array[j]) {
+        const noDwarf = [array[i], array[j]];
+        result = array.filter((number) => !noDwarf.includes(number));
+        // return result;
+      }
+    }
+  }
+
+  console.log(result);
+}
 
 solution([20, 7, 23, 19, 10, 15, 25, 8, 13]);
