50b2b501be3eb6c6aabc12e63f38575ed8bb4ec9
jjanmo <jjanmo07@hanmail.net>
2021-03-29 23:22:18 +0900

    Update section01-05 solution

--- algorithm/section01/s01_05.js
+++ algorithm/section01/s01_05.js
@@ -11,5 +11,15 @@ function solution2(a, b, c, d, e, f, g) {
   return arr[0];
 }
 
+function solution3(array) {
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (min > array[i]) min = array[i];
+  }
+
+  return min;
+}
+
 console.log(solution1(5, 3, 7, 11, 2, 15, 17));
 console.log(solution2(5, 3, 7, 11, 2, 15, 17));
+console.log(solution3([5, 3, 7, 11, 2, 15, 17]));
