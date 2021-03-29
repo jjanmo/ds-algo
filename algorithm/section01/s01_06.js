3fdc0101a94fdc8d43919a97e52c7fcbfef63861
jjanmo <jjanmo07@hanmail.net>
2021-03-29 23:37:47 +0900

    Update section01-06

--- algorithm/section01/s01_06.js
+++ algorithm/section01/s01_06.js
@@ -1,4 +1,4 @@
-function solution(a, b, c, d, e, f, g) {
+function solution1(a, b, c, d, e, f, g) {
   const arr = [a, b, c, d, e, f, g];
   const odd = arr.filter((ele) => ele % 2 !== 0);
 
@@ -11,4 +11,21 @@ function solution(a, b, c, d, e, f, g) {
   console.log(min);
 }
 
-solution(12, 77, 38, 41, 53, 92, 85);
+function solution2(array) {
+  const result = [];
+  let sum = 0;
+  let minOdd = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      sum += array[i];
+      if (minOdd > array[i]) minOdd = array[i];
+    }
+  }
+  result.push(sum);
+  result.push(minOdd);
+
+  return result;
+}
+
+solution1(12, 77, 38, 41, 53, 92, 85);
+console.log(solution2([12, 77, 38, 41, 53, 92, 85]));
