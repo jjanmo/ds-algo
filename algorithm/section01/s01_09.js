3b0ee19d5995a05b264b21f0cab9d20950cfeac3
jjanmo <jjanmo07@hanmail.net>
2021-03-30 23:44:51 +0900

    Update section01-09

--- algorithm/section01/s01_09.js
+++ algorithm/section01/s01_09.js
@@ -1,9 +1,15 @@
-function solution(string) {
+function solution1(string) {
   let newStr = '';
   for (let i = 0; i < string.length; i++) {
     newStr += string[i] === 'A' ? '#' : string[i];
   }
-  console.log(newStr);
+  return newStr;
 }
 
-solution('BANANA');
+// 정규표현식을 이용한 풀이
+function solution2(string) {
+  return string.replace(/A/g, '#');
+}
+
+console.log(solution1('BANANA'));
+console.log(solution2('BANANA'));
