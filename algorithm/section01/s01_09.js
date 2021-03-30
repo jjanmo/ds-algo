78dc8f42a1f0d7d32a291442542872579e1a6491
jjanmo <jjanmo07@hanmail.net>
2021-03-30 23:51:46 +0900

    Update section01-09

--- algorithm/section01/s01_09.js
+++ algorithm/section01/s01_09.js
@@ -8,7 +8,7 @@ function solution1(string) {
 
 // 정규표현식을 이용한 풀이
 function solution2(string) {
-  return string.replace(/A/g, '#');
+  return string.replace(/A/g, '#'); // replace의 결과 변환된 새로운 문자열이 생성
 }
 
 console.log(solution1('BANANA'));
