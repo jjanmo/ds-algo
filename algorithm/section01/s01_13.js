1ab7f1ed87606b18f6b6a37ba49cc218eb9dc01a
jjanmo <jjanmo07@hanmail.net>
2021-05-21 01:51:54 +0900

    Update s01_13 problem

--- algorithm/section01/s01_13.js
+++ algorithm/section01/s01_13.js
@@ -1,4 +1,4 @@
-function solution(string) {
+function solution1(string) {
   const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const LOWERCASE = 'abcdefghijklmmopqrstuvwxyz';
 
@@ -13,7 +13,24 @@ function solution(string) {
     }
   }
 
-  console.log(newStr);
+  return newStr;
 }
 
-solution('StuDY');
+// 처음엔 포맷팅이 익숙해져서 위의 풀이처럼 풀었다. 하지만 include, indexOf는 내부적으로 순회를 하는 것으로 알고 있다.
+// 즉 유사 이중for문이 된다는 것! 그렇기때문에 아래와 같이 푸는 것이 좀 더 명시적이고 깔끔한 것 같다.
+
+function solution2(string) {
+  let result = '';
+  for (let c of string) {
+    if (c === c.toUpperCase()) {
+      result += c.toLowerCase();
+    } else {
+      result += c.toUpperCase();
+    }
+  }
+
+  return result;
+}
+
+console.log(solution1('StuDY'));
+console.log(solution2('StuDY'));
