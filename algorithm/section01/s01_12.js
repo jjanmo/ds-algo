b355bb5578ce6ae6d2d530e70b725f23c84b50a5
jjanmo <jjanmo07@hanmail.net>
2021-05-21 01:36:10 +0900

    Update s01_12 problem

--- algorithm/section01/s01_12.js
+++ algorithm/section01/s01_12.js
@@ -3,3 +3,9 @@ function solution(string) {
 }
 
 solution('ItisTimeToStudy');
+
+/*
+해당 문제는 위의 코드처럼 간단하게 해결이 된다. 하지만 본래 의도는 반복문을 통해서
+소문자를 찾아서 대문자로 변환시켜서 답을 찾는 방법이였다. 또한 아스키코드를 이용해서
+앞선 문제처럼 charCodeAt()를 사용하는 방법도 알아두자
+*/
