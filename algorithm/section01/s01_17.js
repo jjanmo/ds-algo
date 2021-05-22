71452f09d248f0910382db99996ca57687485b14
jjanmo <jjanmo07@hanmail.net>
2021-05-23 00:15:56 +0900

    Update s01_17 problem

--- algorithm/section01/s01_17.js
+++ algorithm/section01/s01_17.js
@@ -1,11 +1,20 @@
-function solution(n, array) {
+function solution1(array) {
   const newArray = [];
 
   array.forEach((string) => {
     if (!newArray.includes(string)) newArray.push(string);
   });
 
-  console.log(newArray);
+  return newArray;
 }
 
-solution(5, ['good', 'time', 'good', 'time', 'student']);
+//앞의 문제처럼 indexOf()를 사용할 수 있다.
+//indexOf()는 문자열과 배열에서 모두 사용할 수 있다. 위의 코드에서 사용한 includes() 역시 둘 다 사용할 수 있다.
+
+function solution2(array) {
+  return array.filter((ele, i) => array.indexOf(ele, i + 1) === -1);
+  // return array.filter((ele, i) => array.indexOf(ele) === i);
+}
+
+console.log(solution1(['good', 'time', 'good', 'time', 'student']));
+console.log(solution2(['good', 'time', 'good', 'time', 'student']));
