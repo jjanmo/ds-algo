8ef111a74616402bcedfa51c093ac3f418bc8450
jjanmo <jjanmo07@hanmail.net>
2021-05-22 23:19:11 +0900

    Update s01_15 problem

--- algorithm/section01/s01_15.js
+++ algorithm/section01/s01_15.js
@@ -1,11 +1,27 @@
-function solution(string) {
+function solution1(string) {
   const midIndex = string.length / 2;
+  let result = '';
+
   if (string.length % 2 === 0) {
-    console.log(string.substring(midIndex - 1, midIndex + 1));
+    result = string.substring(midIndex - 1, midIndex + 1);
   } else {
-    console.log(string[Math.floor(midIndex)]);
+    result = string[Math.floor(midIndex)];
   }
+
+  return result;
 }
 
-solution('study');
-solution('good');
+//같은 코드 다른 모습
+function solution2(string) {
+  const midIndex = Math.floor(string.length / 2);
+
+  return string.length % 2 === 0 ? string.substring(midIndex - 1, midIndex + 1) : string[midIndex];
+}
+
+console.log(solution1('study'));
+console.log(solution1('good'));
+console.log(solution2('study'));
+console.log(solution2('good'));
+
+//string.substring(start, end) => 👍
+//string.substr(start, [length]) => 👎
