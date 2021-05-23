b39addd42b4165d0b221964760c035a79bcdeeae
jjanmo <jjanmo07@hanmail.net>
2021-05-23 13:37:55 +0900

    Update s02_01 problem

--- algorithm/section02/s02_01.js
+++ algorithm/section02/s02_01.js
@@ -1,12 +1,10 @@
-function solution(number, array) {
-    let result = '';
-    for (let i = 0; i < number; i++) {
-        if (i === 0) result += (array[i] + ' ');
-        else {
-            if (array[i] > array[i - 1]) result += (array[i] + ' ');
-        }
-    }
-    console.log(result);
+function solution(array) {
+  let result = [array[0]];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > array[i - 1]) result.push(array[i]);
+  }
+
+  return result;
 }
 
-solution(6, [7, 3, 9, 5, 6, 12]);
+console.log(solution([7, 3, 9, 5, 6, 12]));
