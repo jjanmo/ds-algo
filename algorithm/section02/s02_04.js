7d6e662df2449ced5b515876d81a22bc0be4dae0
jjanmo <jjanmo07@hanmail.net>
2021-05-26 23:40:55 +0900

    Update s02_04 problem

--- algorithm/section02/s02_04.js
+++ algorithm/section02/s02_04.js
@@ -1,7 +1,7 @@
-function solution(problems, answers) {
+function solution(answers) {
   let score = 0;
   let count = 0;
-  for (let i = 0; i < problems; i++) {
+  for (let i = 0; i < answers.length; i++) {
     if (answers[i] === 0) {
       count = 0;
     } else {
@@ -10,7 +10,7 @@ function solution(problems, answers) {
     }
   }
 
-  console.log(score);
+  return score;
 }
 
-solution(10, [1, 0, 1, 1, 1, 0, 0, 1, 1, 0]);
+console.log(solution([1, 0, 1, 1, 1, 0, 0, 1, 1, 0]));
