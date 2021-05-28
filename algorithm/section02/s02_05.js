18bd6d11c47ff13d2689370d374cfa3570cc5413
jjanmo <jjanmo07@hanmail.net>
2021-05-28 23:47:33 +0900

    Update solution

--- algorithm/section02/s02_05.js
+++ algorithm/section02/s02_05.js
@@ -1,16 +1,40 @@
-function solution(studunts, scores) {
-  const array = [...scores].sort((a, b) => b - a);
-  console.log(array);
-  const result = [];
-  for (let i = 0; i < scores.length; i++) {
-    // [87, 92, 92, 100, 92]
-    for (let j = 0; j < array.length; j++) {
-      // [ 100, 92, 92, 92, 87 ]
-      if (scores[i] === array[j]) result.push(j + 1);
+function solution(scores) {
+  // const array = [...scores].sort((a, b) => b - a);
+  // console.log(array);
+  // const result = [];
+  // for (let i = 0; i < scores.length; i++) { //같은 것이 있는지 없는지를 판단해야함
+  //   // [87, 92, 92, 100, 92]
+  //   for (let j = 0; j < array.length; j++) {
+  //     // [ 100, 92, 92, 92, 87 ]
+  //     if (scores[i] === array[j]) {
+  //       array[j] = -1;
+  //       result.push(j + 1);
+  //     }
+  //   }
+  // }
+  // console.log(result.join(' '));
+
+  const sortedArray = [...scores].sort((a, b) => b - a);
+  const ranks = [];
+  const previousIndexes = [];
+  let rank = 0;
+  console.log(sortedArray);
+  for (let i = 0; i < sortedArray.length; i++) {
+    let index;
+    if (sortedArray[i - 1] !== sortedArray[i]) {
+      rank++;
+      index = scores.indexOf(sortedArray[i]);
+    } else {
+      while (previousIndexes.indexOf(sortedArray[i]) !== -1) {
+        index = scores.indexOf(sortedArray[i], index + 1);
+      }
     }
+    console.log(sortedArray[i], index);
+    ranks[index] = rank;
   }
-  console.log(result.join(' '));
+
+  return ranks.join(' ');
 }
 
-// solution(5, [87, 89, 92, 100, 76]);
-solution(5, [87, 92, 92, 100, 92]);
+console.log(solution([87, 89, 92, 100, 76])); // 4 3 2 1 5
+console.log(solution([87, 92, 92, 100, 92])); // 5 2 2 1 2
