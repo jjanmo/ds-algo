fdef1b555df42a71171ffd7a4be4dd22a05cd5ac
jjanmo <jjanmo07@hanmail.net>
2021-05-30 22:29:11 +0900

    Update s02_05 solution

--- algorithm/section02/s02_05.js
+++ algorithm/section02/s02_05.js
@@ -1,40 +1,43 @@
-function solution(scores) {
-  // const array = [...scores].sort((a, b) => b - a);
-  // console.log(array);
-  // const result = [];
-  // for (let i = 0; i < scores.length; i++) { //같은 것이 있는지 없는지를 판단해야함
-  //   // [87, 92, 92, 100, 92]
-  //   for (let j = 0; j < array.length; j++) {
-  //     // [ 100, 92, 92, 92, 87 ]
-  //     if (scores[i] === array[j]) {
-  //       array[j] = -1;
-  //       result.push(j + 1);
-  //     }
-  //   }
-  // }
-  // console.log(result.join(' '));
-
-  const sortedArray = [...scores].sort((a, b) => b - a);
+function solution1(scores) {
+  const sortedArray = [...scores].sort((a, b) => b - a); // 점수 내림차순 정렬
   const ranks = [];
-  const previousIndexes = [];
   let rank = 0;
-  console.log(sortedArray);
+  let index;
   for (let i = 0; i < sortedArray.length; i++) {
-    let index;
-    if (sortedArray[i - 1] !== sortedArray[i]) {
-      rank++;
-      index = scores.indexOf(sortedArray[i]);
+    if (sortedArray[i - 1] === sortedArray[i]) {
+      index = scores.indexOf(sortedArray[i], index + 1);
     } else {
-      while (previousIndexes.indexOf(sortedArray[i]) !== -1) {
-        index = scores.indexOf(sortedArray[i], index + 1);
-      }
+      //달라질 때
+      rank = i + 1;
+      index = scores.indexOf(sortedArray[i]);
     }
-    console.log(sortedArray[i], index);
     ranks[index] = rank;
   }
 
   return ranks.join(' ');
 }
 
-console.log(solution([87, 89, 92, 100, 76])); // 4 3 2 1 5
-console.log(solution([87, 92, 92, 100, 92])); // 5 2 2 1 2
+console.log(solution1([87, 89, 92, 100, 76])); // 4 3 2 1 5
+console.log(solution1([87, 92, 92, 100, 92])); // 5 2 2 1 2
+console.log(solution1([87, 92, 87, 100, 92])); // 4 2 4 1 2
+console.log(solution1([92, 92, 92, 100, 76])); // 2 2 2 1 5
+
+console.log('--------------');
+// 이중for문을 통해서 모두 비교해준다
+function solution2(scores) {
+  const ranks = [];
+  for (let i = 0; i < scores.length; i++) {
+    let rank = 1;
+    for (let j = 0; j < scores.length; j++) {
+      if (scores[i] < scores[j]) rank++;
+    }
+    ranks.push(rank);
+  }
+
+  return ranks.join(' ');
+}
+
+console.log(solution2([87, 89, 92, 100, 76])); // 4 3 2 1 5
+console.log(solution2([87, 92, 92, 100, 92])); // 5 2 2 1 2
+console.log(solution2([87, 92, 87, 100, 92])); // 4 2 4 1 2
+console.log(solution2([92, 92, 92, 100, 76])); // 2 2 2 1 5
