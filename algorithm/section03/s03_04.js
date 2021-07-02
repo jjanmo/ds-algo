cbe2a52e1a8ea1840c8fb725f1de316319632b60
jjanmo <jjanmo07@hanmail.net>
2021-07-02 22:34:36 +0900

    Solve s03-04 problem

---
+++ algorithm/section03/s03_04.js
@@ -0,0 +1,20 @@
+function solution(str, char) {
+  const charIndexList = str
+    .split('')
+    .map((ele, idx) => ele === char && idx)
+    .filter(Boolean);
+
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let min = Number.MAX_SAFE_INTEGER;
+    for (let j = 0; j < charIndexList.length; j++) {
+      const diff = Math.abs(i - charIndexList[j]);
+      if (min > diff) min = diff;
+    }
+    result += min + ' ';
+  }
+
+  return result;
+}
+
+console.log(solution('teachermode', 'e'));
