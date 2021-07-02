aba7f2b44f49ee5dc703317dad3cd7edc5c79d49
jjanmo <jjanmo07@hanmail.net>
2021-07-02 22:06:06 +0900

    Solve s03-03 problem

---
+++ algorithm/section03/s03_03.js
@@ -0,0 +1,8 @@
+function solution(string) {
+  const newString = string.replace(/[a-zA-Z]/g, '');
+
+  return parseInt(newString, 10);
+}
+
+console.log(solution('g0en2T0s8eSoft'));
+console.log(solution('tge0a1h205er'));
