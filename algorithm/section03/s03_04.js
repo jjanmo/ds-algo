d961d1c42c355f3714ae2a8fa3295e1483cc48c8
jjanmo <jjanmo07@hanmail.net>
2021-09-18 23:03:09 +0900

    feat: add/update solution of section3-4

--- algorithm/section03/s03_04.js
+++ algorithm/section03/s03_04.js
@@ -1,4 +1,4 @@
-function solution(str, char) {
+function solution1(str, char) {
   const charIndexList = str
     .split('')
     .map((ele, idx) => ele === char && idx)
@@ -17,4 +17,38 @@ function solution(str, char) {
   return result;
 }
 
-console.log(solution('teachermode', 'e')); // 1 0 1 2 1 0 1 2 2 1 0
+// 생각하지 못한 풀이 👍
+// -> 방향성??으로 접근
+function solution2(str, char) {
+  const answer = [];
+  let distance = Number.MAX_SAFE_INTEGER;
+  // 정방향 탐색(왼쪽에서부터)
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      distance = 0;
+    } else {
+      distance++;
+    }
+    answer.push(distance);
+  }
+
+  // 역방향 탐색(오른쪽에서부터)
+  distance = Number.MAX_SAFE_INTEGER;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] === char) {
+      distance = 0;
+    } else {
+      distance++;
+
+      if (answer[i] > distance) answer[i] = distance;
+    }
+  }
+
+  return answer.join(' ');
+}
+
+console.log(solution1('teachermode', 'e')); // 1 0 1 2 1 0 1 2 2 1 0
+
+console.log('------');
+
+console.log(solution2('teachermode', 'e')); // 1 0 1 2 1 0 1 2 2 1 0
