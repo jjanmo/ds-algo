0333da0932abfc4da98df42f545637615b2248eb
jjanmo <jjanmo07@hanmail.net>
2021-09-18 21:48:25 +0900

    feat: add/update solution of section3-3

--- algorithm/section03/s03_03.js
+++ algorithm/section03/s03_03.js
@@ -1,8 +1,23 @@
-function solution(string) {
+function solution1(string) {
   const newString = string.replace(/[a-zA-Z]/g, '');
 
   return parseInt(newString, 10);
 }
 
-console.log(solution('g0en2T0s8eSoft')); // 208
-console.log(solution('tge0a1h205er')); // 125
+// 직접 찾기
+function solution2(string) {
+  let numString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(string[i])) numString += string[i];
+  }
+
+  return Number(numString);
+}
+
+console.log(solution1('g0en2T0s8eSoft')); // 208
+console.log(solution1('tge0a1h205er')); // 125
+
+console.log('-------');
+
+console.log(solution2('g0en2T0s8eSoft')); // 208
+console.log(solution2('tge0a1h205er')); // 125
