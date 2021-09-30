4d08e4c91d4896d27ebdec64b23733ee26a857ad
jjanmo <jjanmo07@hanmail.net>
2021-09-30 23:24:02 +0900

    feat: add solution of section4-4

---
+++ algorithm/section04/s04_04.js
@@ -0,0 +1,31 @@
+function solution(total, list) {
+  const newList = list;
+  newList.sort((a, b) => a[0] - b[0]);
+
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < newList.length; i++) {
+    const pSum = newList[i][0] + newList[i][1];
+    sum += pSum;
+    if (sum === total) {
+      return count++;
+    } else if (sum < total) {
+      count++;
+    } else {
+    }
+  }
+
+  console.log(sum);
+}
+
+console.log(
+  solution(28, [
+    [6, 6], // 12
+    [2, 2], // 4
+    [4, 3], // 7
+    [4, 5], // 9
+    [10, 3], // 13
+  ])
+);
+
+// [ [상품가격, 배송비], [], []]
