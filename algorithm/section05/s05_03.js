36ec1a4dcd57be5dad638f36b9570537ff49feef
jjanmo <jjanmo07@hanmail.net>
2021-10-13 22:43:27 +0900

    feat: add new solution using two-pointers-algorithm in section5-3

--- algorithm/section05/s05_03.js
+++ algorithm/section05/s05_03.js
@@ -1,4 +1,4 @@
-function solution(array, total) {
+function solution1(array, total) {
   let count = 0;
   for (let i = 0; i < array.length; i++) {
     let sum = array[i];
@@ -12,8 +12,42 @@ function solution(array, total) {
     }
   }
 
-  console.log(count);
+  return count;
 }
 
-console.log(solution([1, 2, 1, 3, 1, 1, 1, 2], 6)); // 3
-console.log(solution([1, 2, 1, 3, 1, 1, 1, 2], 3)); // 4
+function solution2(array, total) {
+  let p1 = 0,
+    count = 0,
+    sum = 0;
+  let p2 = p1 + 1;
+
+  while (p1 !== array.length) {
+    if (sum === total) {
+      count++;
+      p1++;
+      p2 = p1 + 1;
+      sum = 0;
+    } else if (sum > total) {
+      p1++;
+      p2 = p1 + 1;
+      sum = 0;
+    } else {
+      if (p2 < array.length) {
+        sum += array[p2];
+        p2++;
+      } else {
+        p1++;
+        p2 = p1 + 1;
+        sum = 0;
+      }
+    }
+  }
+
+  return count;
+}
+
+console.log(solution1([1, 2, 1, 3, 1, 1, 1, 2], 6)); // 3
+console.log(solution1([1, 2, 1, 3, 1, 1, 1, 2], 3)); // 4
+
+console.log(solution2([1, 2, 1, 3, 1, 1, 1, 2], 6)); // 3
+console.log(solution2([1, 2, 1, 3, 1, 1, 1, 2], 3)); // 4
