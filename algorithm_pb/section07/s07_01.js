881fe0b1cf6f4a9cc16d6999220adf7f4f80b2b5
jjanmo <jjanmo07@hanmail.net>
2021-12-06 10:54:45 +0900

    feat: update solution of section7-1

--- algorithm_pb/section07/s07_01.js
+++ algorithm_pb/section07/s07_01.js
@@ -6,7 +6,7 @@
 3. 맨 처음 위치를 뺀 나머지 리스트를 같은 방법으로 비교하여 교체한다.
 
 */
-function solution(array) {
+function solution1(array) {
   for (let i = 0; i < array.length - 1; i++) {
     let minIndex = i;
     for (let j = i + 1; j < array.length; j++) {
@@ -22,4 +22,41 @@ function solution(array) {
   return array;
 }
 
-console.log(solution([13, 5, 11, 7, 23, 15]));
+console.log(solution1([13, 5, 11, 7, 23, 15])); // [ 5, 7, 11, 13, 15, 23 ]
+
+function solution2(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[minIndex] > array[j]) {
+        minIndex = j;
+      }
+    }
+    [array[minIndex], array[i]] = [array[i], array[minIndex]];
+  }
+
+  return array;
+}
+
+console.log(solution2([13, 5, 11, 7, 23, 15])); // [ 5, 7, 11, 13, 15, 23 ]
+
+/*
+js swap 방법
+
+1. 가장 일반적인 방법
+✅  solution1에서 사용
+임시 변수에 기존값을 담고 그 값을 바꾸고자 하는 곳에 넣어주는 방법
+
+
+2. 구조분해 할당을 통한 방법
+✅ solution2에서 사용
+
+let a = 10;
+let b = 20;
+[b, a] = [a. b]
+
+이 외에도 수학적인 방법(덧셈과 뺄셈 이용), 비트연산 이용 하는 방법이 있지만 이 방법은 비추하는 방법들
+https://dmitripavlutin.com/swap-variables-javascript/
+
+
+*/
