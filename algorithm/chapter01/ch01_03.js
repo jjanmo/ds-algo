3cbfe6e5d60ad6cd823e00642fee839f2bcc0155
jjanmo <jjanmo07@hanmail.net>
2022-02-12 19:15:36 +0900

    feat: add algorithm-ch01-03 file

---
+++ algorithm/chapter01/ch01_03.js
@@ -0,0 +1,26 @@
+/*
+행렬의 곱셈
+
+문제 : 두 n * n 행렬의 곱을 구하시오
+
+2x2 행렬 표현
+[[2, 3],[4, 1]]
+*/
+
+function solution(A, B) {
+  const n = A.length;
+  for (let i = 0; i < n; i++) {}
+}
+
+console.log(
+  solution(
+    [
+      [2, 3],
+      [4, 1],
+    ],
+    [
+      [5, 7],
+      [6, 8],
+    ]
+  )
+);
