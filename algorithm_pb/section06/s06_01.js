e779751e3c98f67e838b722ac6e7f4f7875ca870
jjanmo <jjanmo07@hanmail.net>
2022-03-23 00:57:10 +0900

    feat: fix solution s06-01

--- algorithm_pb/section06/s06_01.js
+++ algorithm_pb/section06/s06_01.js
@@ -1,7 +1,7 @@
-function solution(brackets) {
+function solution1(brackets) {
   const stack = [];
   const bracketArray = brackets.split('');
-  bracketArray.forEach(bracket => {
+  bracketArray.forEach((bracket) => {
     if (bracket === '(') stack.push(bracket);
     else stack.pop(bracket);
   });
@@ -9,5 +9,28 @@ function solution(brackets) {
   return stack.length > 0 ? 'NO' : 'YES';
 }
 
-console.log(solution('(()(()))(()'));
-console.log(solution('(())()()((()))'));
+// console.log(solution1('(()(()))(()')); // NO
+// console.log(solution1('(())()()((()))')); // YES
+// console.log(solution1('(()))')); // NO : 위 풀이의 반례
+
+function solution2(brackets) {
+  const stack = [];
+
+  for (let i = 0; i < brackets.length; i++) {
+    // if (stack.length === 0 && brackets[i] === ')') return 'NO'; // 이 부분을 else에서 체크할 수도 있다.
+    if (brackets[i] === '(') stack.push(brackets[i]);
+    else {
+      const tmp = stack.pop();
+      if (!tmp) return 'NO'; // 빈배열에서 pop()을 하면 undefined를 리턴 : 주석처리한 if와 같은 기능
+    }
+  }
+
+  return stack.length > 0 ? 'NO' : 'YES';
+}
+
+console.log(solution2('(()(()))()')); // YES
+console.log(solution2('(()(()))(()')); // NO
+console.log(solution2('(())()()((()))')); // YES
+console.log(solution2('(()))')); // NO
+console.log(solution2(')')); // NO
+console.log(solution2('(())(()')); // NO
