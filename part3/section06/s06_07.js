83bfa114efdb657c6a1d7ae907f69b2a73aa1c82
jjanmo <jjanmo07@hanmail.net>
2023-03-08 22:39:41 +0900

    feat: add s06-07 other solution

--- part3/section06/s06_07.js
+++ part3/section06/s06_07.js
@@ -8,9 +8,29 @@ function solution(order, mySubjects) {
     }
   }
 
-  return 'YES';
+  return prev === -1 ? 'NO' : 'YES';
 }
 
 console.log(solution('CBA', 'CBDAGE')); // YES
 console.log(solution('CBA', 'FGCDAB')); // NO
 console.log(solution('AFC', 'AFFDCCFF')); // YES
+console.log(solution('ABC', 'EFGHIKD')); // NO
+
+// queue를 이용한 풀이
+function solution2(need, plan) {
+  let needQueue = need;
+  for (let i = 0; i < plan.length; i++) {
+    if (needQueue.includes(plan[i])) {
+      if (needQueue[0] !== plan[i]) return 'NO';
+      else needQueue = needQueue.slice(1);
+    }
+  }
+
+  if (needQueue.length === 0) return 'YES';
+  else return 'NO';
+}
+
+console.log(solution2('CBA', 'CBDAGE')); // YES
+console.log(solution2('CBA', 'FGCDAB')); // NO
+console.log(solution2('AFC', 'AFFDCCFF')); // YES
+console.log(solution2('ABC', 'EFGHIKD')); // NO
