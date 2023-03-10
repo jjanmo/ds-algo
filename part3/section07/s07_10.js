c27b65a8c4a8b43ccfcf7dee377cb702b2d6ecf5
jjanmo <jjanmo07@hanmail.net>
2023-03-10 23:59:35 +0900

    feat: resolve s07-10

--- part3/section07/s07_10.js
+++ part3/section07/s07_10.js
@@ -17,3 +17,21 @@ function solution(n, target, array) {
   return left;
 }
 console.log(solution(8, 32, [23, 87, 65, 12, 57, 32, 99, 81])); // 3
+
+// 다시 풀기
+function solution2(find, array) {
+  const sorted = array.sort((a, b) => a - b);
+  let start = 0;
+  let end = array.length;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (find === sorted[mid]) return mid + 1;
+    else if (find < sorted[mid]) end = mid;
+    else start = mid;
+  }
+
+  return -1;
+}
+
+console.log(solution2(32, [23, 87, 65, 12, 57, 32, 99, 81]));
