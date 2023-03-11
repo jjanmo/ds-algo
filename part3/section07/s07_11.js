3292d090bc74cbe1523b34989ecec52073613617
jjanmo <jjanmo07@hanmail.net>
2023-03-11 23:18:45 +0900

    fix: fix s07-11 solution

--- part3/section07/s07_11.js
+++ part3/section07/s07_11.js
@@ -17,6 +17,8 @@ function solution(n, m, videos) {
   }
 }
 // console.log(solution(9, 3, [1, 2, 3, 4, 5, 6, 7, 8, 9])); // 17
+// console.log(solution2(10, 3, [6, 5, 8, 5, 6, 8, 7, 6, 6, 7])); // 24
+// console.log(solution2(5, 4, [1, 1, 1, 1, 1])); // 2  안되네...✅
 
 // 다시 풀기
 function solution2(N, M, songs) {
@@ -26,8 +28,6 @@ function solution2(N, M, songs) {
   }
 
   let volume = Math.floor(totalSongVolume / M);
-
-  console.log(totalSongVolume, volume);
   let total = 0;
   let count = 0;
 
@@ -46,8 +46,6 @@ function solution2(N, M, songs) {
       }
     }
 
-    console.log('count', volume, count);
-
     if (count === M) return volume;
     else {
       volume++;
@@ -59,4 +57,45 @@ function solution2(N, M, songs) {
 
 // console.log(solution2(9, 3, [1, 2, 3, 4, 5, 6, 7, 8, 9])); // 17
 // console.log(solution2(10, 3, [6, 5, 8, 5, 6, 8, 7, 6, 6, 7])); // 24
-console.log(solution2(5, 4, [1, 1, 1, 1, 1])); // 안되네...✅
+// console.log(solution2(5, 4, [1, 1, 1, 1, 1])); // 2 안되네...✅
+
+// "이분검색"을 이용해야함!!!
+function solution3(N, M, songs) {
+  let answer;
+  let rt = 0;
+  let lt = 0;
+  for (let i = 0; i < N; i++) {
+    rt += songs[i];
+    if (lt < songs[i]) lt = songs[i];
+  }
+
+  while (lt <= rt) {
+    let count = 1;
+    let sum = 0;
+    const mid = parseInt((lt + rt) / 2); // ✅ 포인트1 이진탐색 활용
+
+    for (let i = 0; i < N; i++) {
+      // ✅ 포인트2 비교 방법
+      if (sum + songs[i] > mid) {
+        count++;
+        sum = songs[i];
+      } else {
+        sum += songs[i];
+      }
+    }
+
+    // ✅ 포인트3
+    if (count <= M) {
+      answer = mid;
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution3(9, 3, [1, 2, 3, 4, 5, 6, 7, 8, 9])); // 17
+console.log(solution3(10, 3, [6, 5, 8, 5, 6, 8, 7, 6, 6, 7])); // 24
+console.log(solution3(5, 4, [1, 1, 1, 1, 1])); // 2
