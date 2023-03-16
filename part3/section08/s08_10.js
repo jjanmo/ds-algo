28eb45ecff9859175b7bd3ddcebc526780ae9c9e
jjanmo <jjanmo07@hanmail.net>
2023-03-16 09:14:02 +0900

    feat: solve s08-10, permutation part1

---
+++ part3/section08/s08_10.js
@@ -0,0 +1,30 @@
+// n개 중에서 m개를 뽑는 경우의 수
+function solution(n, m, array) {
+  const visited = Array.from({ length: n }, (_) => false);
+  const tmp = Array.from({ length: m });
+  const result = [];
+  let count = 0;
+
+  const dfs = (level) => {
+    if (level === m) {
+      // 종료
+      count++;
+      result.push([...tmp]);
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      if (visited[i]) continue;
+
+      visited[i] = true;
+      tmp[level] = array[i];
+      dfs(level + 1);
+      visited[i] = false; //?
+    }
+  };
+
+  dfs(0);
+  console.log(result, count);
+}
+
+solution(3, 2, [3, 6, 9]); // 6
+solution(4, 2, [1, 3, 6, 9]); // 12
