c31756825d7c3195e1daad304fef3180b1717704
jjanmo <jjanmo07@hanmail.net>
2023-03-18 22:10:46 +0900

    feat: solve s08-11

---
+++ part3/section08/s08_11.js
@@ -0,0 +1,21 @@
+function solution1(n) {
+  let result = 1;
+  for (let i = 1; i < n + 1; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+function solution2(n) {
+  const recursive = (n) => {
+    if (n === 0) return 1;
+    else return n * recursive(n - 1);
+  };
+
+  console.log(recursive(n));
+}
+
+console.log(solution1(5)); // 120
+
+solution2(5);
