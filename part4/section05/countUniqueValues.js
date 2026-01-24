9b84d5ad82711567d86876d78957931b506dd6ff
jjanmo <jjanmo07@hanmail.net>
2026-01-24 23:38:20 +0900

    refactor: countUniqueValues 에 대한 로직 변경 + 루프를 더 적게 돌도록 변경

--- part4/section05/countUniqueValues.js
+++ part4/section05/countUniqueValues.js
@@ -10,12 +10,11 @@ function countUniqueValues(arr) {
   let ptr2 = 1;
 
   while (ptr2 < arr.length) {
-    if (arr[ptr1] === arr[ptr2]) {
-      ptr2++;
-    } else {
+    if (arr[ptr1] !== arr[ptr2]) {
       uniqueCount++;
       ptr1 = ptr2;
     }
+    ptr2++;
   }
 
   return uniqueCount;
@@ -26,6 +25,7 @@ console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
 console.log(countUniqueValues([])); // 0
 console.log(countUniqueValues([-2, -1, -1, 0, 1])); // 4
 
+console.log('-------');
 
 // Set을 사용하면 더 쉽게 풀수 있지 않나...🥲
 function solution2(arr) {
@@ -36,3 +36,28 @@ console.log(solution2([1, 1, 1, 1, 1, 2])); // 2
 console.log(solution2([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
 console.log(solution2([])); // 0
 console.log(solution2([-2, -1, -1, 0, 1])); // 4
+
+console.log('-------');
+
+// 첫번째 리팩토링 버전
+// ↪ 기존 배열을 변형과 동시에 체크하는 방법 > 공간복잡도가 낮아짐
+function solution3(arr) {
+  if (arr.length === 0) return 0;
+
+  let ptr1 = 0;
+  let ptr2 = 1;
+
+  while (ptr2 < arr.length) {
+    if (arr[ptr1] !== arr[ptr2]) {
+      ptr1++;
+      arr[ptr1] = arr[ptr2];
+    }
+    ptr2++;
+  }
+  return ptr1 + 1;
+}
+
+console.log(solution3([1, 1, 1, 1, 1, 2])); // 2
+console.log(solution3([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+console.log(solution3([])); // 0
+console.log(solution3([-2, -1, -1, 0, 1])); // 4
