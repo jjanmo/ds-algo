f21b8fee483f559f226762d14b6cbac33c3430d9
jjanmo <jjanmo07@hanmail.net>
2026-01-26 23:22:06 +0900

    feat: solve maxSubarraySum pb

---
+++ part4/section05/maxSubarraySum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Array} arr 배열
+ * @param {number} n 연속된 수
+ * @description 배열에서의 연속된 수들 중에서 가장 큰 수를 리턴하는 함수
+ */
+function maxSubarraySum(arr, n) {
+  if (arr.length === 0) return null;
+  let first = 0;
+  let second = 1;
+  let sum = 0;
+
+  while (second < arr.length) {
+    if (second - first === n - 1) {
+      sum = Math.max(
+        arr.slice(first, second + 1).reduce((acc, cur) => acc + cur, 0),
+        sum
+      );
+      second++;
+    } else if (second - first > n - 1) {
+      first++;
+    } else {
+      second++;
+    }
+  }
+  return sum;
+}
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubarraySum([4, 2, 1, 6], 1)); // 6
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubarraySum([], 4)); // null
