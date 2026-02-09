d6bcf9660c854836f89606aa474a215c12d4b463
jjanmo <jjanmo07@hanmail.net>
2026-02-09 23:51:40 +0900

    fix: 엣지 케이스에 대한 조건 추가

--- part4/section06/4_ isSubsequence.js
+++ part4/section06/4_ isSubsequence.js
@@ -1,6 +1,8 @@
 /** 주의 : substring(부분 문자 포함)이 아니라 subsequence(부분 순서) */
 
 function isSubsequence(str1, str2) {
+  if (!str1) return true; // 빈 문자열은 모든 문자열의 부분 서열로 간주(이런 조건이 있는지는 확인되진 않지만, 일반적인 예외 케이스라고함)
+
   let j = 0;
 
   for (let i = 0; i < str2.length; i++) {
